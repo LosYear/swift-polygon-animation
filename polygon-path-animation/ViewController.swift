629ce304e275ede108aa4b0b8d170e02012a2ee4
Yaroslav Losev <losevyaroslav@gmail.com>
2019-09-09 17:24:14 +0300

    Initial Commit

---
+++ polygon-path-animation/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  polygon-path-animation
+//
+//  Created by Yaroslav on 09/09/2019.
+//  Copyright © 2019 Yaroslav. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
